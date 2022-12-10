2e08cd6dbaa9fe3ca1923596d6c1fa47999d881d
Santosh Mohapatra <santoshmohapatra232@gmail.com>
2022-12-11 03:02:37 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import Header from './component/Header.js';
+import Balance from './component/Balance.js';
+import IncomeExpenses from './component/IncomeExpenses.js';
+import TransactionList from './component/TransactionList.js';
+import AddTransaction from './component/AddTransaction.js';
+
+import { GlobalProvider } from './context/GlobalState.js';
+
+function App() {
+  return (
+    <GlobalProvider>
+      <Header />
+      <div className='container'>
+          <Balance />
+          <IncomeExpenses />
+          <TransactionList />
+          <AddTransaction />
+      </div>
+    </GlobalProvider>  
+  );
+}
+
+export default App;
